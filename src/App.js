60b1e4188a05d1e1905855668911de654f45aea6
Tom Greuter <info@tomgreuter.nl>
2021-04-06 21:44:23 +0200

    Working session with Ryan

--- src/App.js
+++ src/App.js
@@ -1,8 +1,193 @@
-import Login from "./Login";
+// import Login from "./Login";
+import React from "react";
 import "./index.scss";
+import {
+  Fade16,
+  Switcher24,
+  Search24,
+  Notification24,
+  UserAvatar24,
+} from "@carbon/icons-react";
+import {
+  HeaderContainer,
+  Header,
+  SkipToContent,
+  HeaderMenuButton,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuItem,
+  HeaderMenu,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  SideNavMenuItem,
+  SideNavLink,
+  // StoryContent,
+  // Search20,
+  // Notification20,
+  // AppSwitcher20,
+  SideNav,
+  SideNavItems,
+  SideNavMenu,
+  HeaderSideNavItems,
+  Switcher,
+  SwitcherItem,
+  HeaderPanel,
+  Grid,
+  Row,
+  Column,
+} from "carbon-components-react";
+import Pie from "./Pie";
+
+// import { SwitcherItemLink } from "carbon-components-react/lib/components/UIShell";
 
 function App() {
-  return <Login></Login>;
+  const action = () => {};
+  const [openSwitcher, setOpenSwitcher] = React.useState(false);
+
+  // return <Login></Login>;
+
+  return (
+    <HeaderContainer
+      render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+        <>
+          <Header aria-label="CloudNatix Platform Name">
+            <SkipToContent />
+            <HeaderMenuButton
+              aria-label="Open menu"
+              onClick={onClickSideNavExpand}
+              isActive={isSideNavExpanded}
+              isCollapsible
+            />
+            <HeaderName href="#" prefix="CloudNatix">
+              [Platform]
+            </HeaderName>
+            <HeaderNavigation aria-label="CloudNatix [Platform]">
+              <HeaderMenuItem href="#">Dashboard</HeaderMenuItem>
+              <HeaderMenuItem href="#">Workload</HeaderMenuItem>
+              <HeaderMenu aria-label="Link 4" menuLinkName="Optimizer">
+                <HeaderMenuItem href="#one">Executive view</HeaderMenuItem>
+                <HeaderMenuItem href="#two">Developer view</HeaderMenuItem>
+                <HeaderMenuItem href="#two">
+                  Business analist view
+                </HeaderMenuItem>
+              </HeaderMenu>
+            </HeaderNavigation>
+            <HeaderGlobalBar>
+              <HeaderGlobalAction
+                aria-label="Search"
+                onClick={action("search click")}
+              >
+                <UserAvatar24 />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction
+                aria-label="Search"
+                onClick={action("search click")}
+              >
+                <Search24 />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction
+                aria-label="Notifications"
+                onClick={action("notification click")}
+              >
+                <Notification24 />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction
+                aria-label="App Switcher"
+                onClick={() => setOpenSwitcher(!openSwitcher)}
+                tooltipAlignment="end"
+              >
+                <Switcher24 />
+              </HeaderGlobalAction>
+            </HeaderGlobalBar>
+
+            <HeaderPanel expanded={openSwitcher}>
+              <Switcher>
+                <SwitcherItem>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+                  quam.
+                  {/* <SwitcherItemLink href="/">Link</SwitcherItemLink> */}
+                </SwitcherItem>
+              </Switcher>
+            </HeaderPanel>
+          </Header>
+
+          <SideNav
+            aria-label="Side navigation"
+            expanded={isSideNavExpanded}
+            isPersistent={false}
+          >
+            <SideNavItems>
+              <HeaderSideNavItems hasDivider={true}>
+                <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
+                <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+                <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+                <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
+                  <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
+                  <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
+                  <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+                </HeaderMenu>
+              </HeaderSideNavItems>
+              <SideNavMenu renderIcon={Fade16} title="Category title">
+                <SideNavMenuItem href="/">Link</SideNavMenuItem>
+                <SideNavMenuItem href="/">Link</SideNavMenuItem>
+                <SideNavMenuItem href="/">Link</SideNavMenuItem>
+              </SideNavMenu>
+              <SideNavMenu renderIcon={Fade16} title="Category title">
+                <SideNavMenuItem href="/">Link</SideNavMenuItem>
+                <SideNavMenuItem href="/">Link</SideNavMenuItem>
+                <SideNavMenuItem href="/">Link</SideNavMenuItem>
+              </SideNavMenu>
+              <SideNavMenu
+                renderIcon={Fade16}
+                title="Category title"
+                isActive={true}
+              >
+                <SideNavMenuItem href="/">Link</SideNavMenuItem>
+                <SideNavMenuItem aria-current="page" href="/">
+                  Link
+                </SideNavMenuItem>
+                <SideNavMenuItem href="/">Link</SideNavMenuItem>
+              </SideNavMenu>
+              <SideNavLink renderIcon={Fade16} href="/">
+                Link
+              </SideNavLink>
+              <SideNavLink renderIcon={Fade16} href="/">
+                Link
+              </SideNavLink>
+            </SideNavItems>
+          </SideNav>
+
+          <div class="bx--grid">
+            <Grid>
+              <Row>
+                <Column>
+                  <h2 style={{ margin: "0 0 130px" }}>Purpose and function</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Similique eveniet aliquam odit totam ab repellat et, alias
+                    aliquid quod hic.
+                  </p>
+                  <Pie></Pie>
+                </Column>
+                <Column>
+                  <p>
+                    repellat cupiditate at, quaerat perspiciatis repudiandae eos
+                    rem assumenda laboriosam illo.
+                  </p>
+                </Column>
+                <Column>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    veritatis ut amet, provident doloremque!
+                  </p>
+                </Column>
+              </Row>
+            </Grid>
+          </div>
+        </>
+      )}
+    />
+  );
 }
 
 export default App;
---
+++ src/Pie.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Pie } from "@ant-design/charts";
+
+const DemoPie: React.FC = () => {
+  var data = [
+    {
+      type: "Field A",
+      value: 27,
+    },
+    {
+      type: "Field B",
+      value: 25,
+    },
+    {
+      type: "分类三",
+      value: 18,
+    },
+    {
+      type: "分类四",
+      value: 15,
+    },
+    {
+      type: "分类五",
+      value: 10,
+    },
+    {
+      type: "其他",
+      value: 5,
+    },
+  ];
+  var config = {
+    appendPadding: 10,
+    data: data,
+    angleField: "value",
+    colorField: "type",
+    color: ["#6929c4", "#1192e8", "#005d5d", "#9f1853", "#fa4d56", "#570408"],
+    radius: 0.9,
+    label: {
+      type: "inner",
+      offset: "-30%",
+      content: function content(_ref) {
+        var percent = _ref.percent;
+        return "".concat(percent * 100, "%");
+      },
+      style: {
+        fontSize: 14,
+        textAlign: "center",
+      },
+    },
+    interactions: [{ type: "element-active" }],
+  };
+  return <Pie {...config} />;
+};
+
+export default DemoPie;
